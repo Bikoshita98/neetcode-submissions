096860efb445cb87d71d2f3a20e5a107f5e7ec82
Bikoshita Gogoi <68753535+Bikoshita98@users.noreply.github.com>
2026-08-06 23:18:05 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let map = {}
+
+        for(let i=0; i<strs.length; i++){
+            let sortedStr = strs[i].split("").sort().join("");
+
+            if(!map[sortedStr]){
+                map[sortedStr] = [strs[i]]
+            } else {
+                map[sortedStr].push(strs[i]);
+            }
+        }
+
+        return [...Object.values(map)];
+    }
+}
