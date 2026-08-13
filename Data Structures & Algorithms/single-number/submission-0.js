0c0ea08892e4d9970e38e28a0630b15c4f5313f0
Bikoshita Gogoi <68753535+Bikoshita98@users.noreply.github.com>
2026-08-14 00:03:42 +0530

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    singleNumber(nums) {
+        let xor = 0;
+        for(let i=0;i<nums.length; i++){
+            xor = xor ^ nums[i];
+        }
+
+        return xor;
+    }
+}
